378ad992279638611244c51113d70157452ae9bc
jubaer919 <60255197+jubaer919@users.noreply.github.com>
2023-10-05 14:22:02 +0600

    showing all the reserved rocket in profile

--- src/Components/MyProfile/Profile.jsx
+++ src/Components/MyProfile/Profile.jsx
@@ -1,9 +1,22 @@
+import { useSelector } from 'react-redux';
+import classes from './Profile.module.css';
+
 export default function Profile() {
+  const rR = useSelector((state) => state.rocket.rocket.filter((rocket) => rocket.reserved));
+
   return (
-    <>
-      <div>
-        <h1>Mission </h1>
+    <div className={classes['profile-container']}>
+      <div className={classes['mission-container']}>
+        <h1>Mission</h1>
       </div>
-    </>
+      <div className={classes['rocket-container']}>
+        <h1>Rocket</h1>
+        <ul>
+          {rR.map((rocketName) => (
+            <li key={Math.random()}>{rocketName.name}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
   );
 }
