94054004b5a8234ea46d0025c461a68e305f2747
HenaryKhuraijam <henarykhu123@gmial.com>
2023-10-05 18:54:55 +0530

    shwoing reserved mission information in profile

--- src/Components/MyProfile/Profile.jsx
+++ src/Components/MyProfile/Profile.jsx
@@ -1,14 +1,35 @@
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMissions } from '../../Redux/missions/missions';
 import classes from './Profile.module.css';
 
 export default function Profile() {
   const rR = useSelector((state) => state.rocket.rocket.filter((rocket) => rocket.reserved));
+  const dispatch = useDispatch();
+  const missions = useSelector((state) => state.missions);
 
+  useEffect(() => {
+    if (!missions.length) dispatch(fetchMissions());
+  }, [dispatch, missions]);
+
+  const myMission = missions.filter((mission) => mission.reserved === true);
   return (
     <div className={classes['profile-container']}>
       <div className={classes['mission-container']}>
-        <h1>Mission</h1>
+        <h2 className="">My Missions</h2>
+        <ul className="">
+          {myMission.length ? (
+            myMission.map((mission) => (
+              <li className="" key={mission.id}>
+                {mission.name}
+              </li>
+            ))
+          ) : (
+            <li className="">None</li>
+          )}
+        </ul>
       </div>
+
       <div className={classes['rocket-container']}>
         <h1>Rocket</h1>
         <ul>
